a89cd29b869ed63bb9b07d935fb8bcc9e755ae34
Yair5675 <yairziv5675@gmail.com>
2025-09-22 18:49:04 +0300

    Create constants representing hashing of Suits using as little bits as possible

--- app/src/main/java/com/example/pokermaster/game_state/Card.java
+++ app/src/main/java/com/example/pokermaster/game_state/Card.java
@@ -1,9 +1,18 @@
 package com.example.pokermaster.game_state;
 
+import android.util.Log;
+
 /**
  * Represents a single card in a poker game.
  */
 public class Card {
+    private static final String TAG = Card.class.getCanonicalName();
+
+    private static final int SPADE_HASH_MASK = 0b00;
+    private static final int HEART_HASH_MASK = 0b01;
+    private static final int DIAMOND_HASH_MASK = 0b10;
+    private static final int CLUB_HASH_MASK = 0b11;
+
     public static final int MAX_RANK = 52;
     public static final int MIN_RANK = 1;
 
@@ -28,5 +37,28 @@ public class Card {
 
     public Suit getSuit() {
         return mSuit;
+    }
+
+    /**
+     * Retrieves the hash mask mapped to the current card's {@link Suit}.
+     * @return The bits used when hashing the suit of the current card.
+     * @throws IllegalStateException If somehow the suit of the current card does not have a hash
+     *                               mask (should never happen but let's crash for fun if it does).
+     */
+    private int getSuitHashMask() {
+        switch (mSuit) {
+            case SPADE:
+                return SPADE_HASH_MASK;
+            case HEART:
+                return HEART_HASH_MASK;
+            case DIAMOND:
+                return DIAMOND_HASH_MASK;
+            case CLUB:
+                return CLUB_HASH_MASK;
+        }
+
+        final String errorMessage = "Unknown suit was inserted to Card object";
+        Log.e(TAG, "getSuitHashMask: " + errorMessage);
+        throw new IllegalStateException(errorMessage);
     }
 }
