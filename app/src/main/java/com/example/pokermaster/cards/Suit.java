536368f9213996cf0c96b61d218a848e217c8c02
Yair5675 <yairziv5675@gmail.com>
2025-10-18 16:48:27 +0300

    Implement toString for Suit

--- app/src/main/java/com/example/pokermaster/cards/Suit.java
+++ app/src/main/java/com/example/pokermaster/cards/Suit.java
@@ -1,5 +1,7 @@
 package com.example.pokermaster.cards;
 
+import androidx.annotation.NonNull;
+
 /**
  * The suit of a poker card
  */
@@ -9,6 +11,11 @@ public enum Suit {
     DIAMOND,
     CLUB
     ;
+
+    private static final String SPADE_SYMBOL = "♠";
+    private static final String HEART_SYMBOL = "♥";
+    private static final String DIAMOND_SYMBOL = "♦";
+    private static final String CLUB_SYMBOL = "♣";
 
     /**
      * Checks if the given suit is a black suit (for example - SPADE).
@@ -24,5 +31,26 @@ public enum Suit {
      */
     public boolean isRed() {
         return this == HEART || this == DIAMOND;
+    }
+
+    /**
+     * {@code toString} implementation for the {@link Suit} class, providing a visually appealing
+     * representation of all suits.
+     * @return The current {@link Suit} variant's pretty string representation.
+     */
+    @NonNull
+    @Override
+    public String toString() {
+        switch (this) {
+            case SPADE:
+                return SPADE_SYMBOL;
+            case HEART:
+                return HEART_SYMBOL;
+            case DIAMOND:
+                return DIAMOND_SYMBOL;
+            case CLUB:
+                return CLUB_SYMBOL;
+        }
+        throw new IllegalStateException("toString not implemented for a Suit enum variant");
     }
 }
