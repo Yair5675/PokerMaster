fc842ce98b7fb0a80de2d90c5a5736b9b6a93476
Yair5675 <yairziv5675@gmail.com>
2025-10-20 23:03:26 +0300

    Add default method isBetterThan to avoid confusion in comparing hands

--- app/src/main/java/com/example/pokermaster/hands/PokerHand.java
+++ app/src/main/java/com/example/pokermaster/hands/PokerHand.java
@@ -2,8 +2,10 @@ package com.example.pokermaster.hands;
 
 /**
  * An abstraction over the type of hands in Texas hold 'em Poker.
- * Note that when comparing two hands, the better one will be considered SMALLER (and its compareTo
- * function will return a negative value).
+ * <p>
+ * Note that when comparing two hands, the better one will be considered SMALLER (and its
+ * {@link Comparable<PokerHand>#compareTo} function will return a negative value).
+ * </p>
  */
 public interface PokerHand extends Comparable<PokerHand> {
     /**
@@ -17,4 +19,19 @@ public interface PokerHand extends Comparable<PokerHand> {
      * @return A number indicating the rank of the implementing poker hand class.
      */
     int getHandRanking();
+
+    /**
+     * Checks if this hand is better than the given one.
+     * <p>
+     *     This function is provided to avoid confusion regarding the {@link Comparable<PokerHand>}
+     *     implementation of {@link PokerHand}. It can be replaced entirely by checking the
+     *     result of {@link Comparable<PokerHand>#compareTo}.
+     * </p>
+     * @param other Another hand which will be compared to the current one.
+     * @return True if the current hand is strictly better than the other (tying hands will return
+     *         false).
+     */
+    default boolean isBetterThan(PokerHand other) {
+        return this.compareTo(other) < 0;
+    }
 }
