77964078a64622279f2e4cc5e1aa69f1d83ed324
Yair5675 <yairziv5675@gmail.com>
2026-01-17 20:41:49 +0200

    Make an annotation for expensive tests, and make gradle skip them unless an "includeExpensive" property is set for the test

---
+++ app/src/test/java/com/example/pokermaster/annotations/ExpensiveTest.java
@@ -0,0 +1,14 @@
+package com.example.pokermaster.annotations;
+
+import org.junit.jupiter.api.Tag;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Tag("expensive")
+public @interface ExpensiveTest {
+}
